36ad90a580bc2b623fb8b3db8d473d948f149264
Katie Pundt <kzpundt@gmail.com>
2022-03-05 13:12:39 -0800

    add navbar links and basic styles

--- my-recipes/src/components/NavBar.js
+++ my-recipes/src/components/NavBar.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// styles
+import './NavBar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/create'>Create Recipe</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/signup'>Sign Up</Link></li>
+        <li><button className='btn'>Logout</button></li>
+      </ul>
+    </div>
   )
 }
