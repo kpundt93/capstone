762e28f61dbb7191de2bc2d004cab169f43ce59b
Katie Pundt <kzpundt@gmail.com>
2022-03-05 16:32:04 -0800

    dynamically render measurement options

--- my-recipes/src/components/ReusableForm.js
+++ my-recipes/src/components/ReusableForm.js
@@ -1,39 +1,65 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+// react-bootstrap
 import Form from 'react-bootstrap/Form'
-
-const categories = [
-  "Breakfast",
-  "Lunch",
-  "Dinner",
-  "Snacks",
-  "Appetizers",
-  "Sweets",
-  "Holiday",
-  "Soups"
-];
-
-const categoryOptions = categories.map((category) => {
-  return (
-    <option key={category} value={category}>{category}</option>
-  );
-});
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 export default function ReusableForm(props) {
+
+  const categories = [
+    "Breakfast",
+    "Lunch",
+    "Dinner",
+    "Snacks",
+    "Appetizers",
+    "Sweets",
+    "Holiday",
+    "Soups"
+  ];
+  
+  const categoryOptions = categories.map((category) => {
+    return (
+      <option key={category} value={category}>{category}</option>
+    );
+  });
+
+  const measurements = [
+    "cup",
+    "tbsp",
+    "tsp",
+    "each",
+    "lb(s)",
+    "oz",
+    "pinch"
+  ];
+
+  const measurementOptions = measurements.map((measurement) => {
+    return (
+      <option key={measurement} value={measurement}>{measurement}</option>
+    );
+  });
+  
+
   return (
     <Form>
       <Form.Group>
         <Form.Label className='form-label'>Title</Form.Label>
         <Form.Control type='text' name='title' required />
       </Form.Group>
-      <Form.Group>
-        <Form.Label className='form-label'>Cook Time</Form.Label>
-        <Form.Control type='text' name='cook-time' required />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label className='form-label'>Servings</Form.Label>
-        <Form.Control type='number' name='servings' step='1' min='1' required />
-      </Form.Group>
+      <Row>
+        <Col>
+          <Form.Label className='form-label'>Cook Time</Form.Label>
+          <Form.Control type='text' name='cook-time' required />
+        </Col>
+        <Col>
+          <Form.Label className='form-label'>Servings</Form.Label>
+          <Form.Control type='number' name='servings' step='1' min='1' required />
+        </Col>
+      </Row>
       <Form.Group>
         <Form.Label className='form-label'>Category</Form.Label>
         <Form.Select>
@@ -41,13 +67,20 @@ export default function ReusableForm(props) {
           { categoryOptions }
         </Form.Select>
       </Form.Group>
-      <Form.Group>
-        <Form.Label className='form-label'>Ingredients</Form.Label>
-        <Form.Control type='text' name='cook-time' required />
-      </Form.Group>
+      <Form.Label>Ingredients</Form.Label>
+      <InputGroup>
+        <Col md='6'><FormControl placeholder='Ingredient' /></Col>
+        <Col md='2'><FormControl placeholder='Qty' /></Col>
+        <Col>
+          <Form.Select>
+            <option>Measurement</option>
+            { measurementOptions }
+          </Form.Select>
+        </Col>
+      </InputGroup>
       <Form.Group>
         <Form.Label className='form-label'>Instructions</Form.Label>
-        <Form.Control type='text' name='cook-time' required />
+        <Form.Control type='text' name='cook-time' as='textarea' rows={3} required />
       </Form.Group>
   </Form>
   )
