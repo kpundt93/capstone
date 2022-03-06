5f5ce15db53f5930494ba7ab8a76615dcb4b1840
Katie Pundt <kzpundt@gmail.com>
2022-03-05 17:37:50 -0800

    WIP - working on inputting muliple ingredients and displaying them

--- my-recipes/src/components/ReusableForm.js
+++ my-recipes/src/components/ReusableForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useState, useRef, useEffect } from 'react'
 // react-bootstrap
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -9,6 +10,17 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 
 export default function ReusableForm(props) {
+  const [title, setTitle] = useState('');
+  const [cookTime, setCookTime] = useState('');
+  const [servings, setServings] = useState('');
+  const [category, setCategory] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [notes, setNotes] = useState('');
+  const [qty, setQty] = useState('');
+  const [measurement, setMeasurement] = useState('');
+  const [newIngredient, setNewIngredient] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const categories = [
     "Breakfast",
@@ -42,45 +54,63 @@ export default function ReusableForm(props) {
       <option key={measurement} value={measurement}>{measurement}</option>
     );
   });
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [...prevIngredients, ing]);
+    }
+    setNewIngredient('');
+
+    ingredientInput.current.focus();
+  }
   
 
   return (
     <Form>
       <Form.Group>
         <Form.Label className='form-label'>Title</Form.Label>
-        <Form.Control type='text' name='title' required />
+        <Form.Control type='text' onChange={(e) => setTitle(e.target.value)} value={title} required />
       </Form.Group>
       <Row>
         <Col>
           <Form.Label className='form-label'>Cook Time</Form.Label>
-          <Form.Control type='text' name='cook-time' required />
+          <Form.Control type='text' onChange={(e) => setCookTime(e.target.value)} value={cookTime} required />
         </Col>
         <Col>
           <Form.Label className='form-label'>Servings</Form.Label>
-          <Form.Control type='number' name='servings' step='1' min='1' required />
+          <Form.Control type='number' step='1' min='1' onChange={(e) => setServings(e.target.value)} value={servings} required />
         </Col>
       </Row>
       <Form.Group>
         <Form.Label className='form-label'>Category</Form.Label>
-        <Form.Select>
+        <Form.Select onChange={(e) => setCategory(e.target.value)} value={category}>
           <option>Choose a category</option>
           { categoryOptions }
         </Form.Select>
       </Form.Group>
       <Form.Label>Ingredients</Form.Label>
       <InputGroup>
-        <Col md='6'><FormControl placeholder='Ingredient' /></Col>
-        <Col md='2'><FormControl placeholder='Qty' /></Col>
-        <Col>
-          <Form.Select>
+        <Col md='6'><FormControl placeholder='Ingredient' onChange={(e) => setNewIngredient(e.target.value)} value={newIngredient} ref={ingredientInput}/></Col>
+        <Col md='2'><FormControl placeholder='Qty' onChange={(e) => setQty(e.target.value)} value={qty} /></Col>
+        <Col md='3'>
+          <Form.Select onChange={(e) => setMeasurement(e.target.value)} value={measurement}>
             <option>Measurement</option>
             { measurementOptions }
           </Form.Select>
         </Col>
+        <Col md="1"><button className="btn" onClick={handleAdd}>+</button></Col>
       </InputGroup>
+      <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p> 
       <Form.Group>
         <Form.Label className='form-label'>Instructions</Form.Label>
-        <Form.Control type='text' name='cook-time' as='textarea' rows={3} required />
+        <Form.Control type='text' as='textarea' rows={3} onChange={(e) => setInstructions(e.target.value)} value={instructions} required />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className='form-label'>Notes</Form.Label>
+        <Form.Control type='text' as='textarea' rows={2} onChange={(e) => setNotes(e.target.value)} value={notes} />
       </Form.Group>
   </Form>
   )
