40b085d92d7f24aa1288a9d5ad8074d4e68ba480
Katie Pundt <kzpundt@gmail.com>
2022-03-06 20:46:48 -0800

    write custom useSignup hook

---
+++ my-recipes/src/hooks/useSignup.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+import { projectAuth } from '../firebase-config'
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // sign up user
+      const response = await projectAuth.createUserWithEmailAndPassword(email, password);
+      console.log(response.user);
+      // if sign up fails, throw error
+      if(!response) {
+        throw new Error('Could not complete sign up.');
+      }
+    }
+    catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+};
