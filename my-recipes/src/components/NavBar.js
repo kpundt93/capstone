7f0bd36047b266b47eff7e3ac22e11aa82c6a3c7
Katie Pundt <kzpundt@gmail.com>
2022-03-07 13:11:00 -0800

    import and use useLogout hook to sign users out

--- my-recipes/src/components/NavBar.js
+++ my-recipes/src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 // styles
 import './NavBar.css'
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <div className='navbar'>
       <ul>
@@ -11,7 +14,7 @@ export default function Navbar() {
         <li><Link to='/create'>Create Recipe</Link></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/signup'>Sign Up</Link></li>
-        <li><button className='btn'>Logout</button></li>
+        <li><button className='btn' onClick={logout}>Logout</button></li>
       </ul>
     </div>
   )
