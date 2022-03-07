6825c36d0f5a09dead0afc0e76df15fbfdcc599d
Katie Pundt <kzpundt@gmail.com>
2022-03-06 20:47:58 -0800

    update state in try/catch block

--- my-recipes/src/hooks/useSignup.js
+++ my-recipes/src/hooks/useSignup.js
@@ -14,10 +14,14 @@ export const useSignup = () => {
       // sign up user
       const response = await projectAuth.createUserWithEmailAndPassword(email, password);
       console.log(response.user);
+
       // if sign up fails, throw error
       if(!response) {
         throw new Error('Could not complete sign up.');
       }
+
+      setIsPending(false);
+      setError(null);
     }
     catch (err) {
       console.log(err.message);
