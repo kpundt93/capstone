3d33c7705d674d3b03479b20845cb6f217fa8bf3
Katie Pundt <kzpundt@gmail.com>
2022-03-07 14:22:04 -0800

    prevent user from being logged out upon browser refresh

--- my-recipes/src/components/App.js
+++ my-recipes/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
 // components
 import NavBar from './NavBar'
 import Home from './Home'
@@ -11,27 +12,31 @@ import Signup from './Signup'
 import './App.css'
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className='app'>
-    <BrowserRouter>
-      <SideBar />
-      <div className='container'>
-        <NavBar />
-        <Switch />
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path="/create">
-            <CreateRecipe />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-        </div>
-    </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <SideBar />
+          <div className='container'>
+            <NavBar />
+            <Switch />
+              <Route exact path='/'>
+                <Home />
+              </Route>
+              <Route path="/create">
+                <CreateRecipe />
+              </Route>
+              <Route path='/login'>
+                <Login />
+              </Route>
+              <Route path='/signup'>
+                <Signup />
+              </Route>
+            </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
