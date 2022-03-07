d6b1ff82917d0ab20f82a826aabbcdd4dabe0ea7
Katie Pundt <kzpundt@gmail.com>
2022-03-07 13:58:11 -0800

    conditionally render nav links based on logged in status

--- my-recipes/src/components/NavBar.js
+++ my-recipes/src/components/NavBar.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 // styles
 import './NavBar.css'
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className='navbar'>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/create'>Create Recipe</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/signup'>Sign Up</Link></li>
-        <li><button className='btn' onClick={logout}>Logout</button></li>
+        {/* only show these links if a user is not logged in */}
+        {!user && (
+          <>
+            <li><Link to='/login'>Login</Link></li>
+            <li><Link to='/signup'>Sign Up</Link></li>
+          </>
+        )}
+        {/* only show these if a user is logged in */}
+        {user && (
+          <>
+            <li><Link to='/create'>Create Recipe</Link></li>
+            <li><button className='btn' onClick={logout}>Logout</button></li>
+          </>
+        )}
       </ul>
     </div>
   )
