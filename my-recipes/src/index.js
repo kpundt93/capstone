fe3950332a2e9b483af3f5f37e0424888a216db1
Katie Pundt <kzpundt@gmail.com>
2022-03-06 21:35:27 -0800

    wrap App in AuthContext

--- my-recipes/src/index.js
+++ my-recipes/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContext.Provider>
+      <App />
+    </AuthContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
