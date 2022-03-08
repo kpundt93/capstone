6409af84d30374e501d1796e58e302d2c114185d
Katie Pundt <kzpundt@gmail.com>
2022-03-07 18:19:20 -0800

    delete ReusableForm

--- my-recipes/src/components/Home.js
+++ my-recipes/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Home() {
+  // need to do something with the uid when rendering recipe cards to only show recipes belong to the currently logged in.
+  const { user } = useAuthContext();
   return (
     <div>Home</div>
   )
