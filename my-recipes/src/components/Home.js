33f1a45a829e6a400c2381d3dc569ac5b33768dc
Katie Pundt <kzpundt@gmail.com>
2022-03-07 22:08:52 -0800

    fetch recipes from Firestore and render on home page

--- my-recipes/src/components/Home.js
+++ my-recipes/src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useAuthContext } from '../hooks/useAuthContext'
+import { useCollection } from '../hooks/useCollection'
+// components
+import RecipeList from './RecipeList'
 
 export default function Home() {
   // need to do something with the uid when rendering recipe cards to only show recipes belong to the currently logged in.
   const { user } = useAuthContext();
+  const {documents, error } = useCollection('recipes');
+
   return (
-    <div>Home</div>
+    <React.Fragment>
+      <h2>Home</h2>
+      <div>
+        {error && <p>{error}</p>}
+        {documents && <RecipeList recipes={documents} />}
+      </div>
+    </React.Fragment>
   )
 }
--- my-recipes/src/components/RecipeList.js
+++ my-recipes/src/components/RecipeList.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function RecipeList() {
+export default function RecipeList({ recipes }) {
   return (
-    <div>RecipeList</div>
+    <div className='recipe-list'>
+      {recipes.map(recipe => (
+        <div key={recipe.id} className='recipe-card'>
+          <h4>{recipe.title}</h4>
+          <p>Cook time: {recipe.cookTime}</p>
+          <p>Servings: {recipe.servings}</p>
+          <p>Category: {recipe.category}</p>
+          <Link to={`/recipes/${recipe.id}`}>Details</Link>
+        </div>
+      ))}
+    </div>
   )
 }
