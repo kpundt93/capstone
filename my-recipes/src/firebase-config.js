52b3dad24bd8a27758d62ef2f90189049d11c40e
Katie Pundt <kzpundt@gmail.com>
2022-03-09 15:47:09 -0800

    initialize Firebase Storage for image uploads

--- my-recipes/src/firebase-config.js
+++ my-recipes/src/firebase-config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCzxt025q1HD1ypkJLuYtDDeTZinyY5PUs",
@@ -18,5 +19,6 @@ firebase.initializeApp(firebaseConfig);
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 const timestamp = firebase.firestore.Timestamp;
+const projectStorage = firebase.storage()
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
