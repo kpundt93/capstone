0b115292604213f26dc2dce5ed1437b4a62c282f
Katie Pundt <kzpundt@gmail.com>
2022-03-10 10:47:53 -0800

    style RecipeDetails

--- my-recipes/src/components/RecipeDetails.js
+++ my-recipes/src/components/RecipeDetails.js
@@ -3,6 +3,10 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { projectFirestore } from '../firebase-config'
 import { Link } from 'react-router-dom'
+// styles
+import './RecipeDetails.css'
+// images
+import PlaceholderImg from '../img/generic-recipe.jpg'
 
 export default function Recipe() {
   const { id } = useParams();
@@ -31,26 +35,27 @@ export default function Recipe() {
   }, [id]);
 
   return (
-    <div>
+    <div className="recipe-details">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
       {recipe && (
         <>
-          <h2>{recipe.title}</h2>
-          <p>Cook Time: {recipe.cookTime}</p>
-          <p>Servings: {recipe.servings}</p>
-          <p>Category: {recipe.category}</p>
-          <p>Ingredients:</p>
+          <h3>{recipe.title}</h3>
+          <img src={PlaceholderImg} alt="a plate of food" />
+          <p><strong>Cook Time:</strong>  {recipe.cookTime}</p>
+          <p><strong>Servings:</strong>  {recipe.servings}</p>
+          <p><strong>Category:</strong>  {recipe.category}</p>
+          <p><strong>Ingredients:</strong> </p>
             <ul>
             {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
             </ul>
-          <p>Instructions: <br />
+          <p><strong>Instructions:</strong>  <br />
             {recipe.instructions}
           </p>
-          <p>Notes: <br />
+          <p id='notes'><strong>Notes:</strong>  <br />
             {recipe.notes}
           </p>
-          <Link to={`/edit/${docToEdit}`}>Edit</Link>
+          <Link to={`/edit/${docToEdit}`} className="edit-link">Edit</Link>
         </>
       )}
     </div>
