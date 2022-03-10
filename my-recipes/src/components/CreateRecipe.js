0ec44c76461aac6d108b814cadd3abe2e4b7121c
Katie Pundt <kzpundt@gmail.com>
2022-03-10 09:08:04 -0800

    style create form

--- my-recipes/src/components/CreateRecipe.js
+++ my-recipes/src/components/CreateRecipe.js
@@ -3,11 +3,17 @@ import { useState, useEffect } from 'react'
 import { useFirestore } from '../hooks/useFirestore'
 import { useAuthContext } from '../hooks/useAuthContext'
 import { timestamp } from '../firebase-config'
+import { Link } from 'react-router-dom'
 // react-bootstrap
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+// styles
+import './Form.css'
+// images
+import AddIcon from '../img/add_circle.svg'
+import RemoveIcon from '../img/remove-icon.svg'
 
 export default function CreateRecipe({ uid }) {
   const { addDocument, response } = useFirestore('recipes');
@@ -102,60 +108,66 @@ export default function CreateRecipe({ uid }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label className='form-label'>Title</Form.Label>
-        <Form.Control type='text' onChange={e => setForm({...form, title: e.target.value})} value={setForm.title} required />
-      </Form.Group>
+    <div className="recipe-form">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label className='form-label'>Title</Form.Label>
+          <Form.Control type='text' onChange={e => setForm({...form, title: e.target.value})} value={setForm.title} required />
+        </Form.Group>
 
-      <Row>
-        <Col>
-          <Form.Label className='form-label'>Cook Time</Form.Label>
-          <Form.Control type='text' onChange={(e) => setForm({...form, cookTime: e.target.value})} required />
-        </Col>
+        <Row>
+          <Col>
+            <Form.Label className='form-label'>Cook Time</Form.Label>
+            <Form.Control type='text' onChange={(e) => setForm({...form, cookTime: e.target.value})} required />
+          </Col>
 
-        <Col>
-          <Form.Label className='form-label'>Servings</Form.Label>
-          <Form.Control type='number' step='1' min='1' onChange={(e) => setForm({...form, servings: e.target.value})} required />
-        </Col>
-      </Row>
+          <Col>
+            <Form.Label className='form-label'>Servings</Form.Label>
+            <Form.Control type='number' step='1' min='1' onChange={(e) => setForm({...form, servings: e.target.value})} required />
+          </Col>
+        </Row>
 
-      <Form.Group>
-        <Form.Label className='form-label'>Category</Form.Label>
-        <Form.Select onChange={(e) => setForm({...form, category: e.target.value})}>
-          <option>Choose a category</option>
-          { categoryOptions }
-        </Form.Select>
-      </Form.Group>
+        <Form.Group>
+          <Form.Label className='form-label'>Category</Form.Label>
+          <Form.Select onChange={(e) => setForm({...form, category: e.target.value})}>
+            <option>Choose a category</option>
+            { categoryOptions }
+          </Form.Select>
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Ingredients</Form.Label>
-        { 
-          form.ingredients.map((ingredient, i) => (
-          <Form.Control type="text" key={i} value={ingredient} onChange={(e) => handleIngredient(e, i)} />
-          ))
-        }
-        <button className='btn btn-secondary' onClick={handleIngredientCount}>Add ingredient</button>
-        <button className='btn btn-danger' onClick={handleRemoveIngredient}>Remove ingredient</button>
-      </Form.Group>
+        <Form.Group>
+          <br />
+          <Form.Label className='form-label'>Ingredients</Form.Label>
+          { 
+            form.ingredients.map((ingredient, i) => (
+            <Form.Control type="text" key={i} value={ingredient} onChange={(e) => handleIngredient(e, i)} />
+            ))
+          }
+          <div id="ingredients-buttons">
+            <Link className='ingredient-btn' id='add' onClick={handleIngredientCount}><img src={AddIcon} alt='Add' />Add</Link>
+            <Link className='ingredient-btn' id='remove' onClick={handleRemoveIngredient}><img src={RemoveIcon} alt='Remove' />Remove</Link>
+          </div>
+          <br />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label className='form-label'>Instructions</Form.Label>
-        <Form.Control type='text' as='textarea' rows={3} onChange={(e) => setForm({...form, instructions: e.target.value})} required />
-      </Form.Group>
+        <Form.Group>
+          <Form.Label className='form-label'>Instructions</Form.Label>
+          <Form.Control type='text' as='textarea' rows={3} onChange={(e) => setForm({...form, instructions: e.target.value})} required />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label className='form-label'>Notes</Form.Label>
-        <Form.Control type='text' as='textarea' rows={2} onChange={(e) => setForm({...form, notes: e.target.value})} />
-      </Form.Group>
-      
-      {/* <Form.Group>
-        <Form.Label className='form-label'>Image</Form.Label>
-        <Form.Control type='file' onChange={handleFileChange} contentEditable />
-        {imageError && <p>{imageError}</p>}
-      </Form.Group> */}
+        <Form.Group>
+          <Form.Label className='form-label'>Notes</Form.Label>
+          <Form.Control type='text' as='textarea' rows={2} onChange={(e) => setForm({...form, notes: e.target.value})} />
+        </Form.Group>
+        
+        {/* <Form.Group>
+          <Form.Label className='form-label'>Image</Form.Label>
+          <Form.Control type='file' onChange={handleFileChange} contentEditable />
+          {imageError && <p>{imageError}</p>}
+        </Form.Group> */}
 
-      <Button className="btn btn-primary" type="submit" onClick={(e) => setForm({...form, uid: user.uid, createdAt: timestamp.fromDate(new Date())})}>Create Recipe</Button>
-  </Form>
+        <button className="recipe-submit" type="submit" onClick={(e) => setForm({...form, uid: user.uid, createdAt: timestamp.fromDate(new Date())})}>Create Recipe</button>
+    </Form>
+  </div>
   );
 }
